10c9500c2d6ab4453af5cfba1488854a73609d29
Manolo Canales <keikurono@users.noreply.github.com>
2019-11-05 21:45:39 -0500

    Update traductorVideo.sh
    removing comments

--- traductorVideo.sh
+++ traductorVideo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-	#echo "Error, ingress two arguments: ./translateVideo texto_file video_file output_name";
 	exit 1;
 fi
 texto=$1;
@@ -16,19 +15,13 @@ ffvideo=$3;
 cat $texto | egrep -v "^.{1,2}:.{1,2}" | sed -r 's/[¿]/\ /g' | sed -r 's/[«»“”]/"/g' | iconv -f utf-8 -t iso-8859-1  > "$ftexto"
 
 text2wave "$ftexto" -o "$faudio"
-#espeak -ves-la+m2 -f $ftexto -s140 -w $faudio
 
 r=$(echo $(ffprobe -v error -show_entries format=duration   -of default=noprint_wrappers=1:nokey=1 "$faudio")/$(ffprobe -v error -show_entries format=duration   -of default=noprint_wrappers=1:nokey=1 "$video")|bc -l)
-echo "++++++++++++++++++++++++++++++++++";
+
 ffmpeg -i "$video" -vcodec copy -an "$fvideo" -y < /dev/null
-echo "++++++++++++++++++++++++++++++++++";
-
 ffmpeg -i "$fvideo" -filter_complex "[0:v]setpts=$r*PTS[v]" -map "[v]" "$fvideosd" -y < /dev/null
-
-echo "++++++++++++++++++++++++++++++++++";
 ffmpeg -i "$fvideosd" -i $faudio -vcodec copy "$ffvideo" -y < /dev/null
 
-echo "++++++++++++++++++++++++++++++++++";
 echo "Video translated created: '$ffvideo'"; 
 
 rm -f "$faudio" "$fvideo" "$fvideosd" "$fftexto" "$ftexto"
